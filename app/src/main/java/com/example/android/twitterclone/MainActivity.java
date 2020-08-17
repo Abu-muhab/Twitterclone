69ec8c37fc9a204888682f7e152139d8b4a7a24e
Abumuhab <abumuhab98@gmail.com>
2020-08-17 08:22:40 +0100

    fragnent management

--- app/src/main/java/com/example/android/twitterclone/MainActivity.java
+++ app/src/main/java/com/example/android/twitterclone/MainActivity.java
@@ -7,15 +7,19 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
     private Page currentPage;
+    int currentToolBarId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolBar);
+        Toolbar toolbar = findViewById(R.id.home_toolbar);
+        currentToolBarId = R.id.home_toolbar;
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setElevation(4);
@@ -36,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         TimelineFragment timelineFragment = new TimelineFragment();
         if (currentPage != Page.TIMELINE) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, timelineFragment).commit();
+            replaceToolBar(R.layout.home_toolbar, currentToolBarId);
+            currentToolBarId = R.id.home_toolbar;
             currentPage = Page.TIMELINE;
         }
     }
@@ -43,9 +49,20 @@ public class MainActivity extends AppCompatActivity {
     public void searchButtonClicked(View view) {
         SearchFragment searchFragment = new SearchFragment();
         if (currentPage != Page.SEARCH) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, searchFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, searchFragment, "search").commit();
+            replaceToolBar(R.layout.search_toolbar, currentToolBarId);
+            currentToolBarId = R.id.search_toolbar;
             currentPage = Page.SEARCH;
         }
+    }
+
+    void replaceToolBar(int newToolBarLayoutId, int oldToolBarId) {
+        Toolbar toolbar = findViewById(oldToolBarId);
+        ViewGroup parent = (ViewGroup) toolbar.getParent();
+        int index = parent.indexOfChild(toolbar);
+        parent.removeView(toolbar);
+        toolbar = (Toolbar) getLayoutInflater().inflate(newToolBarLayoutId, parent, false);
+        parent.addView(toolbar, index);
     }
 }
 
